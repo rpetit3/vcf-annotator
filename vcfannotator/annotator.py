93b8b6ee8399ce79f2bcf77273a62578da6010e4
Robert A Petit III <robert.petit@emory.edu>
2014-08-27 13:11:15 -0400

    Added descritions of annotations in the INFO section

--- vcfannotator/annotator.py
+++ vcfannotator/annotator.py
@@ -3,21 +3,50 @@ from vcfannotator import vcftools
 from Bio.Seq import Seq
 
 class Annotator(object):
-    def __init__(self, gb_file=False, vcf_file=False):
+    def __init__(self, gb_file=False, vcf_file=False, length=15):
         self.__gb = genbank.GenBank(gb_file)
         self.__vcf = vcftools.VCFTools(vcf_file)
-        self.__gb.base_by_pos(1731)
-        self.__gb.base_by_pos(1732)
+        #self.__gb.base_by_pos(1731)
+        #self.__gb.base_by_pos(1732)
+        self.add_annotation_info()
        
+    def add_annotation_info(self):
+        self.__vcf.add_information_fields([
+            ['RefCodon', None, 'String', 'Reference codon'],
+            ['AltCodon', None, 'String', 'Alternate codon'],
+            ['RefAminoAcid', None, 'String', 'Reference amino acid'],
+            ['AltAminoAcid', None, 'String', 'Alternate amino acid'],
+            ['CodonPosition', '1', 'Integer', 'Codon position in the gene'],
+            ['SNPCodonPosition', '1', 'Integer', 'SNP position in the codon'],
+            ['AminoAcidChange', None, 'String', 'Amino acid change'],
+            ['Substitution', '1', 'Integer', '0:synonymous, 1:nonsynonymous'],
+            ['LocusTag', None, 'String', 'Locus tag associated with gene'],
+            ['GI', None, 'String', 'GI number associated with gene'],
+            ['GeneName', None, 'String', 'Name of gene'],
+            ['GeneDescription', None, 'String', 'Description of gene'],
+            ['Comments', None, 'String', 'Example: Negative strand: T->C'],
+            ['FlankingRegion', None, 'String', "Sequence of flanking region"],
+        ])
+        
     def annotate_vcf_records(self):
+        self.__vcf.print_metadata()
+        self.__vcf.print_filter()
+        self.__vcf.print_format()
+        self.__vcf.print_info()
+        self.__vcf.print_contigs()
+        
         for record in self.__vcf.records:
             self.__gb.index = record.POS
             record_annotation = self.__gb.annotation_by_position()
             
             if len(record.ALT) > 1:
-                print 'Multiple SNPs ({0}->{1})\t{2}'.format(record.REF, record.ALT,record_annotation)
+                print 'Multiple SNPs ({0}->{1})\t{2}'.format(
+                    record.REF, record.ALT, record_annotation
+                )
             elif len(record.ALT[0]) > 1 or len(record.REF) > 1:
-                print 'Indel ({0}->{1})\t{2}'.format(record.REF, record.ALT[0],record_annotation)
+                print 'Indel ({0}->{1})\t{2}'.format(
+                    record.REF, record.ALT[0], record_annotation
+                )
             else:
                 if record_annotation[1] is not None:
                     codon_info = self.__gb.codon_by_position(record.POS)
@@ -29,9 +58,11 @@ class Annotator(object):
                     ref_aa = codon_info[0].translate()
                     alt_aa = Seq(''.join(alt_codon)).translate()
                     
-                    print '{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}{7}{8}'.format(record.POS, record.REF, 
-                                                 record.ALT, codon_info[0], ''.join(alt_codon), 
-                                                 codon_info[1], ref_aa, codon_info[2], alt_aa)
+                    print '{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}{7}{8}'.format(
+                        record.POS, record.REF, record.ALT, codon_info[0], 
+                        ''.join(alt_codon), codon_info[1], ref_aa, 
+                        codon_info[2], alt_aa
+                    )
                 else:
                     print record.POS, record.REF, record.ALT, 'Intergenic'
 
--- vcfannotator/vcftools.py
+++ vcfannotator/vcftools.py
@@ -1,9 +1,16 @@
+import collections
 import vcf
 
 class VCFTools(object):
     def __init__(self, vcf_file):
-        self.__vcf_reader = vcf.Reader(open(vcf_file, 'r'))
-        self.records = [record for record in self.__vcf_reader]
+        self.reader = vcf.Reader(open(vcf_file, 'r'))
+        self.records = [record for record in self.reader]
+        self.number_conversion = {
+            '.': None,  # Unknown number of values
+            'A': -1,  # Equal to the number of alleles in a given record
+            'G': -2,  # Equal to the number of genotypes in a given record
+        }
+        self._Info = collections.namedtuple('Info', ['id', 'num', 'type', 'desc'])
         
     def print_vcf_records(self):
         for record in self.records:
@@ -11,3 +18,74 @@ class VCFTools(object):
             
     def variant_positions(self):
         return [record.POS for record in self.records]
+        
+        
+    def add_information_fields(self, info_list):
+        for i in info_list:
+            id, num, type, desc = i
+            self.__add_information_field(id, num, type, desc)
+            
+    def __add_information_field(self, id, num, type, desc):
+        if id:
+            self.reader.infos[id] = self._Info(id, num, type, desc)
+        
+    def convert_number(self, number):
+        '''
+            Covert PyVCF's way of storing Number fields back to VCF standard
+
+            https://github.com/jamescasbon/PyVCF/blob/master/vcf/parser.py            
+        '''
+        if number is None:
+            return '.'
+        elif number == -1:
+            return 'A'
+        elif number == -2:
+            return 'G'
+        else:
+            return number
+          
+    def print_metadata(self):
+        for k,v in self.reader.metadata.items():
+            print '##{0}={1}'.format(k,v)
+            
+    def print_filter(self):
+        '''
+        Example: ##FILTER=<ID=LowConf,Description="DP < 10|| QD < 2.5">
+        '''
+        for i in self.reader.filters:
+            print '##FILTER=<ID={0},Description="{1}">'.format(
+                self.reader.filters[i].id, self.reader.filters[i].desc
+            )
+            
+    def print_format(self):
+        '''
+        Example: ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+        '''
+        for i in self.reader.formats:
+            print '##FORMAT=<ID={0},Number={1},Type={2},Description="{3}">'.format(
+                self.reader.formats[i].id, 
+                self.convert_number(self.reader.formats[i].num), 
+                self.reader.formats[i].type, 
+                self.reader.formats[i].desc
+            )
+    def print_info(self):
+        '''
+        Example: ##INFO=<ID=MQ,Number=1,Type=Float,Description="RMS Mapping Quality">
+        '''
+        for i in self.reader.infos:
+            print '##INFO=<ID={0},Number={1},Type={2},Description="{3}">'.format(
+                self.reader.infos[i].id, 
+                self.convert_number(self.reader.infos[i].num), 
+                self.reader.infos[i].type, 
+                self.reader.infos[i].desc
+            )
+            
+    def print_contigs(self):
+        '''
+        Example: ##contig=<ID=gi|289163350|ref|NC_013774.1|,length=1767>
+        '''
+        for i in self.reader.contigs:
+            print '##contig=<ID={0},length={1}>'.format(
+                self.reader.contigs[i].id, 
+                self.reader.contigs[i].length 
+            )
