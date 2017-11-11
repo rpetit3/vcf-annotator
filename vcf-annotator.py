f6421ca45c1db836b31a67b8d8be478a8ff73a7b
Robert A Petit III <robbie.petit@gmail.com>
2017-11-11 08:19:30 -0500

    combined classes and main script, made adjustments to make it python3 compatible

--- vcf-annotator.py
+++ vcf-annotator.py
@@ -1,18 +1,20 @@
-"""Annotate a given VCF file according to the reference GenBank."""
-from vcfannotator import genbank
-from vcfannotator import vcftools
+#! /usr/bin/env python3
+"""Annotate a given VCF file according to the given reference GenBank file."""
+import collections
+from Bio import SeqIO
 from Bio.Seq import Seq
+import vcf
 
 
 class Annotator(object):
-    """Annotator class."""
+    """Annotate a given VCF file according to the reference GenBank."""
 
     def __init__(self, gb_file=False, vcf_file=False):
         """Initialize variables."""
         self.__annotated_features = ["CDS", "tRNA", "rRNA", "ncRNA",
                                      "misc_feature"]
-        self.__gb = genbank.GenBank(gb_file)
-        self.__vcf = vcftools.VCFTools(vcf_file)
+        self.__gb = GenBank(gb_file)
+        self.__vcf = VCFTools(vcf_file)
         self.add_annotation_info()
 
     def add_annotation_info(self):
@@ -169,3 +171,187 @@ class Annotator(object):
     def write_vcf(self, output='/dev/stdout'):
         """Write the VCF to the specified output."""
         self.__vcf.write_vcf(output)
+
+
+class GenBank(object):
+    """A class for parsing GenBank files."""
+
+    def __init__(self, gb=False):
+        """Inititalize variables."""
+        self.__gb = SeqIO.read(open(gb, 'r'), 'genbank')
+        self._index = None
+        self.feature = None
+        self.features = ["CDS", "rRNA", "tRNA", "ncRNA", "repeat_region",
+                         "misc_feature"]
+        self.build_position_index()
+        self.gene_codons = {}
+
+    @property
+    def index(self):
+        """Postion index for features."""
+        return self._index
+
+    @index.setter
+    def index(self, value):
+        self._index = self.__position_index[value - 1]
+        self.__set_feature()
+
+    def build_position_index(self):
+        """Create a index for feature by position."""
+        self.__position_index = [None] * len(self.__gb.seq)
+        for i in range(len(self.__gb.features)):
+            if self.__gb.features[i].type in self.features:
+                start = int(self.__gb.features[i].location.start)
+                end = int(self.__gb.features[i].location.end)
+                self.__position_index[start:end] = [i] * (end - start)
+
+    def __set_feature(self):
+        if self._index is None:
+            self.feature_exists = False
+            self.feature = None
+        else:
+            self.feature_exists = True
+            self.feature = self.__gb.features[self._index]
+
+    def codon_by_position(self, pos):
+        """Retreive the codon given a postion of a CDS feature."""
+        if self._index not in self.gene_codons:
+            self.split_into_codons()
+        gene_position = self.position_in_gene(pos)
+        codon_position = gene_position // 3
+        return [self.gene_codons[self._index][codon_position],
+                gene_position % 3,
+                codon_position + 1]
+
+    def split_into_codons(self):
+        """Split the complete CDS feature in to a list of codons."""
+        start = self.feature.location.start
+        end = self.feature.location.end
+        seq = ''.join(list(self.__gb.seq[start:end]))
+
+        if self.feature.strand == -1:
+            seq = Seq(seq).reverse_complement()
+
+        self.gene_codons[self._index] = [
+            seq[i:i + 3] for i in range(0, len(seq), 3)
+        ]
+
+    def position_in_gene(self, pos):
+        """Return a codon postion in a gene."""
+        if self.feature.strand == 1:
+            return pos - self.feature.location.start - 1
+        else:
+            return self.feature.location.end - pos
+
+    def base_by_pos(self, pos):
+        """Print the base by position."""
+        print(self.__gb.seq[pos - 1])
+
+    def determine_iupac_base(self, bases):
+        """
+        Determine the IUPAC symbol for a list of nucleotides.
+
+        Source: https://en.wikipedia.org/wiki/Nucleic_acid_notation
+        List elements are in this order: [A,C,G,T]
+        """
+        if len(bases) > 1:
+            iupac_notation = {
+                'W': [True, False, False, True],
+                'S': [False, True, True, False],
+                'M': [True, True, False, False],
+                'K': [False, False, True, True],
+                'R': [True, False, True, False],
+                'Y': [False, True, False, True],
+                'B': [False, True, True, True],
+                'D': [True, False, True, True],
+                'H': [True, True, False, True],
+                'V': [True, True, True, False],
+                'N': [False, False, False, False]
+            }
+
+            base_condition = [base in bases for base in ['A', 'C', 'G', 'T']]
+            for symbol, iupac_condition in iupac_notation.items():
+                if iupac_condition == base_condition:
+                    return symbol
+
+    def is_transition(self, ref_base, alt_base):
+        """
+        Identify SNP as being a transition or not.
+
+        1: Transition, 0:Transversion
+        """
+        substitution = ref_base + alt_base
+        transition = ['AG', 'GA', 'CT', 'TC']
+
+        if substitution in transition:
+            return 1
+        else:
+            return 0
+
+
+class VCFTools(object):
+    """A class for parsing VCF formatted files."""
+
+    def __init__(self, vcf_file):
+        """Initialize variables."""
+        self.reader = vcf.Reader(open(vcf_file, 'r'))
+        self.records = [record for record in self.reader]
+
+    def add_information_fields(self, info_list):
+        """Add a given list of information fields to the VCF."""
+        for i in info_list:
+            id, num, type, desc = i
+            self.__add_information_field(id, num, type, desc)
+
+    def __add_information_field(self, id, num, type, desc):
+        """Add a given information field to the VCF."""
+        _Info = collections.namedtuple('Info', ['id', 'num', 'type', 'desc'])
+        if id:
+            self.reader.infos[id] = _Info(id, num, type, desc)
+
+    def write_vcf(self, output='/dev/stdout'):
+        """Write the VCF to a given output file."""
+        vcf_writer = vcf.Writer(open(output, 'w'), self.reader)
+        for record in self.records:
+            vcf_writer.write_record(record)
+
+
+if __name__ == '__main__':
+    import argparse as ap
+    import os
+    import sys
+
+    parser = ap.ArgumentParser(
+        prog='vcf-annotator.py',
+        conflict_handler='resolve',
+        description=("Annotate SNPs from a VCF file using the reference "
+                     "genome's GenBank file.")
+    )
+    parser.add_argument('vcf', metavar="VCF_FILE", type=str,
+                        help='VCF file of SNPs')
+    parser.add_argument('gb', metavar="GENBANK_FILE", type=str,
+                        help='GenBank file of the reference genome.')
+    parser.add_argument('--output', metavar="STRING", type=str,
+                        default='/dev/stdout',
+                        help='File to write VCF output to (Default STDOUT).')
+    parser.add_argument('-h', '--help', action='help',
+                        help='Show this help message and exit')
+
+    if len(sys.argv) == 1:
+        parser.print_usage()
+        sys.exit(1)
+
+    args = parser.parse_args()
+    # Verify input exists
+    if not os.path.isfile(args.gb):
+        print('Unable to locate GenBank file: {0}'.format(args.gb))
+        parser.print_usage()
+        sys.exit(2)
+    elif not os.path.isfile(args.vcf):
+        print('Unable to locate VCF file: {0}'.format(args.vcf))
+        parser.print_usage()
+        sys.exit(1)
+
+    annotator = Annotator(gb_file=args.gb, vcf_file=args.vcf)
+    annotator.annotate_vcf_records()
+    annotator.write_vcf(args.output)
